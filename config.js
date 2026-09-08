4bd5b46ce00142c988e39cee9bd8518c73f2ffb8
ujja5772 <ujja5772@gmail.com>
2026-09-08 22:41:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,5 @@
 // 아래 2줄을 본인의 Supabase 프로젝트 정보로 바꿔주세요. (README.md 참고)
 // 이 파일 하나만 고치면 두 페이지 모두에 적용돼요.
 
-const SUPABASE_URL = 'https://krmsgpgondjbgxgivlhy.supabase.co/rest/v1/
-';
+const SUPABASE_URL = 'https://krmsgpgondjbgxgivlhy.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_zx8xn87ziclyA4PuILsgjg_p5q_TsB3';
